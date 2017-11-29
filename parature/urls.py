32caf57ca0d508901dcff4d99e56cf3bf6d4dae1
Nick Bennett <nick271828@gmail.com>
2017-11-28 19:05:07 -0600

    Super basic search functionality.

--- parature/urls.py
+++ parature/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ticket_list, name='ticket_list'),
     url(r'^ticket/(?P<pk>\d+)/$', views.ticket_detail, name='ticket_detail'),
+    url(r'^search-form/$', views.search_form, name='search_form'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- parature/views.py
+++ parature/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import TicketDetails, TicketHistory
 
+
 def ticket_list(request):
-    tickets = TicketDetails.objects.all()[:10]
+    tickets = TicketDetails.objects.order_by('datecreated')[:20]
     return render(request, 'parature/ticket_list.html', {'tickets': tickets})
 
 def ticket_detail(request, pk):
     ticket = get_object_or_404(TicketDetails, pk=pk)
     return render(request, 'parature/ticket_detail.html', {'ticket': ticket})
+
+def search_form(request):
+    return render(request, 'parature/search_form.html')
+
+def search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        tickets = TicketDetails.objects.filter(details__contains=q).order_by('datecreated')
+        return render(request, 'parature/ticket_list.html', {'tickets': tickets, 'query': q})
+    else:
+        return HttpResponse('Please submit a search term.')
