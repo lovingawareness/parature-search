5a883a71c0c2bb40936785871c5d7d13b68bc631
Nick Bennett <nick271828@gmail.com>
2017-11-29 22:08:06 -0600

    Polished up interface, consolidated views and URLs.

--- parature/urls.py
+++ parature/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ticket_list, name='ticket_list'),
+    url(r'^$', views.index, name='index'),
     url(r'^ticket/(?P<pk>\d+)/$', views.ticket_detail, name='ticket_detail'),
-    url(r'^search-form/$', views.search_form, name='search_form'),
-    url(r'^search/$', views.search, name='search'),
 ]
--- parature/views.py
+++ parature/views.py
@@ -3,21 +3,14 @@ from django.http import HttpResponse
 from .models import TicketDetails, TicketHistory
 
 
-def ticket_list(request):
-    tickets = TicketDetails.objects.order_by('datecreated')[:20]
-    return render(request, 'parature/ticket_list.html', {'tickets': tickets})
-
 def ticket_detail(request, pk):
     ticket = get_object_or_404(TicketDetails, pk=pk)
     return render(request, 'parature/ticket_detail.html', {'ticket': ticket})
 
-def search_form(request):
-    return render(request, 'parature/search_form.html')
-
-def search(request):
+def index(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        tickets = TicketDetails.objects.filter(details__contains=q).order_by('datecreated')
-        return render(request, 'parature/ticket_list.html', {'tickets': tickets, 'query': q})
+        tickets = TicketDetails.objects.filter(details__contains=q).order_by('-datecreated')
+        return render(request, 'parature/ticket_list_with_search.html', {'tickets': tickets, 'query': q})
     else:
-        return HttpResponse('Please submit a search term.')
+        return render(request, 'parature/ticket_list_with_search.html')
