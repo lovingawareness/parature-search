c2434eb5f45c8b00aebeeb1fc4d90d8439d6cbc8
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2017-12-15 17:22:59 -0600

    asdf

--- parature/urls.py
+++ parature/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/tickets/', views.ticket_search, name='ticket_search'),
     url(r'^search/customers/', views.customer_search, name='customer_search'),
+    url(r'^search/comment_search_by_csr/', views.comment_search_by_csr, name='comment_search_by_csr'),
     url(r'^ticket/(?P<pk>\d+)/$', views.ticket_detail, name='ticket_detail'),
     url(r'^customer/(?P<pk>\d+)/$', views.customer_detail, name='customer_detail'),
+    url(r'^comment/(?P<pk>\d+)/$', views.comment_detail, name='comment_detail'),
 ]
--- parature/views.py
+++ parature/views.py
@@ -14,10 +14,14 @@ def customer_detail(request, pk):
     tickets = customer.ticketdetails_set.all()
     return render(request, 'parature/customer_detail.html', {'customer': customer, 'tickets': tickets})
 
+def comment_detail(request, pk):
+    comment = get_object_or_404(TicketHistory, pk=pk)
+    return render(request, 'parature/comment_detail.html', {'comment': comment})
+
 def ticket_search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        tickets = TicketDetails.objects.filter(details__contains=q).order_by('-datecreated')
+        tickets = TicketDetails.objects.filter(details__icontains=q).order_by('id')
         return render(request, 'parature/ticket_list_with_search.html', {'tickets': tickets, 'query': q})
     else:
         return render(request, 'parature/ticket_list_with_search.html')
@@ -25,7 +29,16 @@ def ticket_search(request):
 def customer_search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        customers = Customer.objects.filter(netid__contains=q).order_by('netid')
+        customers = Customer.objects.filter(netid__icontains=q).order_by('netid')
         return render(request, 'parature/customer_list_and_search.html', {'customers': customers, 'query': q})
     else:
         return render(request, 'parature/customer_list_and_search.html')
+
+
+def comment_search_by_csr(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        comments = TicketHistory.objects.filter(performed_by_csr__icontains=q).filter(action_name='Post Internal Comment').order_by('id')
+        return render(request, 'parature/comments_by_csr.html', {'comments': comments, 'query': q})
+    else:
+        return render(request, 'parature/comments_by_csr.html')
