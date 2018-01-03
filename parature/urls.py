b40b0edf3d1a6ddf3354d9c6f6028f7ddcb0ba13
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-03 23:13:44 +0000

    Redirect the root to the search URL.

--- parature/urls.py
+++ parature/urls.py
@@ -1,7 +1,9 @@
+from django.views.generic import RedirectView
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='search/')),
     url(r'^search/', views.ticket_search, name='ticket_search'),
     url(r'^ticket/(?P<pk>\d+)/$', views.ticket_detail, name='ticket_detail'),
     url(r'^customer/(?P<pk>\d+)/$', views.customer_detail, name='customer_detail'),
