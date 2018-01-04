04b70a464e763789f5d6064469cbb91711a543b3
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-04 20:16:35 +0000

    Search on multiple ticket fields. TODO: Fix search so that it doesn't result in a 502 when searching all fields (ticket_history, ticket_solution, ticket_details, ticket_summary). It does work with searching just ticket_history or searching ticket_summary, ticket_details, and ticket_solution. It's a start.

--- parature/views.py
+++ parature/views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import operator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
@@ -23,7 +25,20 @@ def comment_detail(request, pk):
 def ticket_search(request):
     if 'q' in request.GET and request.GET['q']:
         query = request.GET['q']
-        tickets = TicketDetails.objects.filter(Q(details__icontains=query)).order_by('-id')
+        search_fields = {'ticket_details': request.GET.get('search_ticket_details') == 'on',
+                        'ticket_summary': request.GET.get('search_ticket_summary') == 'on',
+                        'ticket_solution': request.GET.get('search_ticket_solution') == 'on',
+                        'ticket_comments': request.GET.get('search_ticket_history') == 'on'}
+        query_filters = []
+        if search_fields['ticket_details']:
+            query_filters.append(Q(details__icontains=query))
+        if search_fields['ticket_summary']:
+            query_filters.append(Q(summary__icontains=query))
+        if search_fields['ticket_solution']:
+            query_filters.append(Q(solution__icontains=query))
+        if search_fields['ticket_comments']:
+            query_filters.append(Q(tickethistory__comments__icontains=query))
+        tickets = TicketDetails.objects.filter(reduce(operator.or_, query_filters)).distinct().order_by('-id')
         return render(request, 'parature/ticket_search.html', {'tickets': tickets, 'query': query})
     else:
         return render(request, 'parature/ticket_search.html')
