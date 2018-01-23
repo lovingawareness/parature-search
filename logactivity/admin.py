bb8f10ad8c18a3936e668529375691f99c0b6a42
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-23 15:31:11 -0600

    Customize admin view of the log records.

--- logactivity/admin.py
+++ logactivity/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Record
 
-admin.site.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    readonly_fields = ('path', 'query_string', 'user', 'user_address')
+    list_filter = ['created_at', 'user']
+    list_display = ('path_full', 'user', 'user_address', 'created_at')
+
+    def view_on_site(self, obj):
+        return obj.path_full
+
+admin.site.register(Record, RecordAdmin)
