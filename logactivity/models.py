c18d570f191320bd04a31cbf0eab043288a549f1
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-23 15:42:15 +0000

    Fix logactivity model so that it allows for non-authenticated users.

--- logactivity/models.py
+++ logactivity/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     path = models.TextField()
     user_address = models.GenericIPAddressField(null=True)
     query_string = models.TextField(null=True)
