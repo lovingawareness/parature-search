518d5b5078d5ce4a9c9448cb1a3cec9baa489566
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-23 15:30:52 -0600

    Change logactivity model to use CharField instead of TextField for URL parts.

--- logactivity/models.py
+++ logactivity/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    path = models.TextField()
+    path = models.CharField(max_length=256)
     user_address = models.GenericIPAddressField(null=True)
-    query_string = models.TextField(null=True)
+    query_string = models.CharField(max_length=256, null=True)
+
+    @property
+    def path_full(self):
+        return self.path + ('?' + self.query_string if self.query_string else '')
 
     def __str__(self):
-        return self.path + (self.query_string or '') + ' accessed by ' + str(self.user or '')
+        return self.path_full + ' accessed by ' + str(self.user or '') + ' at ' + str(self.created_at)
