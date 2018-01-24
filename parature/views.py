6ed73e422b10c09cf505b4d1153cabd96900142f
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-24 16:12:56 -0600

    Add paginated list of solved tickets to CSR detail page.

--- parature/views.py
+++ parature/views.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import operator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
@@ -45,10 +46,25 @@ def csr_detail(request, csr):
     Q_csr = Q(performed_by_csr__exact=csr)
     Q_comment = Q(action_name__exact='Post External Comment') | Q(action_name__exact='Post Internal Comment')
     Q_solution = Q(action_name__exact='Solve')
+
     solved_count = TicketHistory.objects.filter(Q_solution, Q_csr).values_list('ticket_id', flat=True).distinct().count()
     commented_count = TicketHistory.objects.filter(Q_comment, Q_csr).values_list('ticket_id', flat=True).distinct().count()
     touched_count = TicketHistory.objects.filter(Q_csr).values_list('ticket_id', flat=True).distinct().count()
+
     oldest_action = TicketHistory.objects.filter(Q_csr).filter(action_date__isnull=False).earliest('action_date')
     newest_action = TicketHistory.objects.filter(Q_csr).filter(action_date__isnull=False).latest('action_date')
-    return render(request, 'parature/csr.html', {'csr': csr, 'solved_count': solved_count, 'commented_count': commented_count, 'touched_count': touched_count, 'oldest_action': oldest_action, 'newest_action': newest_action})
+
+    tickets_solved_list = TicketDetails.objects.filter(Q(assignedto__exact=csr)).order_by('-ticketid')
+    paginator = Paginator(tickets_solved_list, 10) # show 10 tickets per page
+    page = request.GET.get('page')
+    try:
+        tickets_solved = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        tickets_solved = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver last page of results.
+        tickets_solved = paginator.page(paginator.num_pages)
+
+    return render(request, 'parature/csr.html', {'csr': csr, 'solved_count': solved_count, 'commented_count': commented_count, 'touched_count': touched_count, 'oldest_action': oldest_action, 'newest_action': newest_action, 'tickets_solved': tickets_solved})
 
