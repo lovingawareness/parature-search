72445ab19267a8e9f62190b4e3a6463f2d64a85d
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-01-31 14:59:31 -0600

    Add histogram of tickets solved to CSR detail view.

--- parature/views.py
+++ parature/views.py
@@ -44,6 +44,7 @@ def csr_list(request):
 
 def csr_detail(request, csr):
     Q_csr = Q(performed_by_csr__exact=csr)
+    Q_csr_assigned = Q(assignedto__exact=csr)
     Q_comment = Q(action_name__exact='Post External Comment') | Q(action_name__exact='Post Internal Comment')
     Q_solution = Q(action_name__exact='Solve')
 
@@ -54,7 +55,10 @@ def csr_detail(request, csr):
     oldest_action = TicketHistory.objects.filter(Q_csr).filter(action_date__isnull=False).earliest('action_date')
     newest_action = TicketHistory.objects.filter(Q_csr).filter(action_date__isnull=False).latest('action_date')
 
-    tickets_solved_list = TicketDetails.objects.filter(Q(assignedto__exact=csr)).order_by('-ticketid')
+    tickets_solved_list = TicketDetails.objects.filter(Q_csr_assigned).order_by('-ticketid')
+
+    ticket_created_dates = tickets_solved_list.values_list('datecreated', flat=True)
+
     paginator = Paginator(tickets_solved_list, 10) # show 10 tickets per page
     page = request.GET.get('page')
     try:
@@ -66,5 +70,5 @@ def csr_detail(request, csr):
         # if page is out of range (e.g. 9999), deliver last page of results.
         tickets_solved = paginator.page(paginator.num_pages)
 
-    return render(request, 'parature/csr.html', {'csr': csr, 'solved_count': solved_count, 'commented_count': commented_count, 'touched_count': touched_count, 'oldest_action': oldest_action, 'newest_action': newest_action, 'tickets_solved': tickets_solved})
+    return render(request, 'parature/csr.html', {'csr': csr, 'solved_count': solved_count, 'commented_count': commented_count, 'touched_count': touched_count, 'oldest_action': oldest_action, 'newest_action': newest_action, 'tickets_solved': tickets_solved, 'ticket_created_dates': ticket_created_dates})
 
