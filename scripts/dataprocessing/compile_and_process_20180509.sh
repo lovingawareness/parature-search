969a432f82e10c88d91d79167fe05823fe59a9f4
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-05-10 11:32:27 -0500

    Updated scripts folder for final data load.

--- scripts/dataprocessing/compile_and_process_20180509.sh
+++ scripts/dataprocessing/compile_and_process_20180509.sh
@@ -9,7 +9,7 @@ CURRENT_FOLDER=$ROOT_FOLDER/current
 # This is where the $HISTORICAL_FOLDER and current data dumps are combined into single CSV files
 COMPILED_FOLDER=$ROOT_FOLDER/compiled
 # This is the date string found in the current data dump
-DATESTRING=20180122
+DATESTRING=20180509
 # Copy the customer file from the $CURRENT_FOLDER folder to the compiled folder
 cp $CURRENT_FOLDER/customer_$DATESTRING.csv $COMPILED_FOLDER/
 # Remove BOM
@@ -17,14 +17,15 @@ echo Removing BOM...
 echo Processing customer data file.
 tail -c +4 $COMPILED_FOLDER/customer_$DATESTRING.csv > $COMPILED_FOLDER/customer_$DATESTRING-noBOM.csv
 echo Processing the $HISTORICAL_FOLDER ticket details files.
-tail -c +4 $HISTORICAL_FOLDER/ticket_details_20121231-20000101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20121231-20000101-noBOM.csv
+tail -c +4 $HISTORICAL_FOLDER/ticket_details_20111231-20070101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20111231-20070101-noBOM.csv
+tail -c +4 $HISTORICAL_FOLDER/ticket_details_20121231-20120101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20121231-20120101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_details_20131231-20130101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20131231-20130101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_details_20141231-20140101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20141231-20140101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_details_20151231-20150101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20151231-20150101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_details_20161231-20160101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20161231-20160101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_details_20171231-20170101.csv > $HISTORICAL_FOLDER/noBOM/ticket_details_20171231-20170101-noBOM.csv
 echo Processing the $HISTORICAL_FOLDER ticket history files.
-tail -c +4 $HISTORICAL_FOLDER/ticket_history_20111231-20000101.csv > $HISTORICAL_FOLDER/noBOM/ticket_history_20111231-20000101-noBOM.csv
+tail -c +4 $HISTORICAL_FOLDER/ticket_history_20111231-20070101.csv > $HISTORICAL_FOLDER/noBOM/ticket_history_20111231-20070101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_history_20121231-20120101.csv > $HISTORICAL_FOLDER/noBOM/ticket_history_20121231-20120101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_history_20131231-20130101.csv > $HISTORICAL_FOLDER/noBOM/ticket_history_20131231-20130101-noBOM.csv
 tail -c +4 $HISTORICAL_FOLDER/ticket_history_20141231-20140101.csv > $HISTORICAL_FOLDER/noBOM/ticket_history_20141231-20140101-noBOM.csv
@@ -40,9 +41,10 @@ python compile_csvs_from_historical_and_current.py $DATESTRING $HISTORICAL_FOLDE
 # Dedupe the columns in the customer data file
 echo Deduping columns in customer data file...
 python dedupe_customer_csv.py $COMPILED_FOLDER/customer_$DATESTRING-noBOM.csv $COMPILED_FOLDER/customer_$DATESTRING-noBOM-deduped.csv
-# Remove the RETIRED columns from ticket_details data file
-echo Removing RETIRED columns from ticket_details data file...
-csvcut -c 1-15,78-105 $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM.csv > $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM-trimmed.csv
+echo Here are the columns with numbers in ticket details:
+csvcut -n $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM.csv
+echo Removing RETIRED columns and "Maintenance/Task Type" column from ticket_details data file...
+csvcut -c 1-8,10-16,79-107 $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM.csv > $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM-trimmed.csv
 # Reset the blank customerIDs in ticket_details to an existing customerID
 echo Resetting blank customerIDs in ticket_details to an existing customerID
 python unblank_customer_ids_in_details.py $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM-trimmed.csv $COMPILED_FOLDER/ticket_details_$DATESTRING-noBOM-trimmed-noblanks.csv
--- scripts/dataprocessing/drop_imported_tables.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo -u postgres psql -d postgres -a -f ./drop_imported_tables.sql
