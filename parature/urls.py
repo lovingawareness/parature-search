7802e61dfb9ebd26ee81923c1670b01241f8dd2e
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2018-05-16 15:26:04 -0500

    Add view to show non-closed loaner tickets.

--- parature/urls.py
+++ parature/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^search/tickets/', views.ticket_search, name='ticket_search'),
     url(r'^search/customers/', views.customer_search, name='customer_search'),
     url(r'^ticket/(?P<pk>\d+)/$', views.ticket_detail, name='ticket_detail'),
+    url(r'^loaners/$', views.loaner_tickets, name='loaner_tickets'),
     url(r'^customer/(?P<pk>\d+)/$', views.customer_detail, name='customer_detail'),
     url(r'^comment/(?P<pk>\d+)/$', views.comment_detail, name='comment_detail'),
     url(r'^csrs/$', views.csr_list, name='csr_list'),
